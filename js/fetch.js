8c6ce84fed3dd3e97ae98b5d735c8502e184d0dc
Cody R Fishback <cfishback1@gmail.com>
2023-11-30 10:09:13 -0600

    added davids walkthrough

--- js/fetch.js
+++ js/fetch.js
@@ -4,10 +4,20 @@ const publicCommit = function getCommit(username) {
     return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {"authorization": "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"}})
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(data)})
 
 
-        });
+                .then(events =>{
+                    for(let event of events){
+                        if(event.type === "PushEvent"){
+                            console.log(event.created_at);
+                            return event.payload.commits[0].url
+                        }
+                    }
+                })
+
+
+
 }
 
 console.log(publicCommit("TheCosmicFish"))
