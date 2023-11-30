c0afacf8decf82f46b89e9e19be9117132c07c0d
Cody R Fishback <cfishback1@gmail.com>
2023-11-30 09:26:07 -0600

    finished the ajax store

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,17 @@
-
-
 //------------------------------------------------------------------------------------------
+
+const publicCommit = function getCommit(username) {
+    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {"authorization": "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"}})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+
+        });
+}
+
+console.log(publicCommit("TheCosmicFish"))
+
+
+
 
